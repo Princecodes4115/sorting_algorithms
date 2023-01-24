98039f09ce4677423fb570e567b70f24509b4aef
Ezekiel Moses <codewithprince.com@gmail.com>
2023-01-24 00:54:07 -0800

    update on task 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,66 +1,111 @@
 #include "sort.h"
+#include <stdio.h>
 /**
- * swap - sorts an array of integers in ascending order
- * @array: elements to be sorted
- * @i: left element
- * @j: right element
- * @size: size of array
+ *_calloc - this is a calloc function
+ *@nmemb: number of elemets
+ *@size: bit size of each element
+ *Return: pointer to memory assignement
  */
-void swap(int *array, int i, int j, int size)
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int aux;
+	unsigned int i = 0;
+	char *p;
 
-	if (array[i] == array[j])
-		return;
-	aux = array[i];
-	array[i] = array[j];
-	array[j] = aux;
-	print_array(array, size);
+	if (nmemb == 0 || size == 0)
+		return ('\0');
+	p = malloc(nmemb * size);
+	if (p == '\0')
+		return ('\0');
+	for (i = 0; i < (nmemb * size); i++)
+		p[i] = '\0';
+	return (p);
 }
 /**
- * partition - sort array partition
- * @array: elements to be sorted
- * @start: left side
- * @end: right side
- * @size: size of array
- * Return: pivot sorted
- */
-int partition(int *array, int start, int end, int size)
+ *merge - make a merge
+ *@arr: one from start to mid
+ *@tmp: temp array used in merge, was created outside to
+ *optimize reducing the system calls
+ *@start: first element position
+ *@mid: array middle
+ *@end: last element position
+ **/
+void merge(int *arr, int *tmp, int start, int mid, int end)
 {
-	int pivot = array[end];
-	int split_idx = start;
-	int j;
+	/*  sizes and temp arrays */
+	int size_left = mid - start + 1, size_right = end - mid;
+	int *array_left, *array_right;
+	/* counters */
+	int left, right, i = 0;
 
-	for (j = start; j < end; j++)
-		if (array[j] < pivot)
-			swap(array, split_idx++, j, size);
-	swap(array, split_idx, end, size);
-	return (split_idx);
-}
-/**
- * recursive - quick_sort core
- * @array: elements to be sorted
- * @start: left side
- * @end: right side
- * @size: size of array
- */
-void recursive(int *array, int start, int end, int size)
-{
-	if (start < end)
+	array_left = &tmp[0];
+	array_right = &tmp[size_right];
+	for (left = 0; left < size_left; left++)
+		array_left[left] = arr[start + left];
+	for (right = 0; right < size_right; right++)
+		array_right[right] = arr[mid + 1 + right];
+	left = 0, right = 0, i = start;
+	/* merging tmp arrays into main array*/
+	while (left < size_left && right < size_right)
 	{
-		int pivot_pos = partition(array, start, end, size);
+		if (array_left[left] <= array_right[right])
+			arr[i] = array_left[left], left++;
+		else
+			arr[i] = array_right[right], right++;
+		i++;
+	}
+	/* merging remaining left array into main array*/
+	while (left < size_left)
+		arr[i] = array_left[left], left++, i++;
+	/* merging remaining right array into main array*/
+	while (right < size_right)
+		arr[i] = array_right[right], right++, i++;
+	printf("Merging...\n");
+	printf("[left]: ");
+	print_array(array_left, left);
+	printf("[right]: ");
+	print_array(array_right, right);
+	printf("[Done]: ");
+	print_array(&arr[start], left + right);
+}
+/**
+ *mergesort - function that sorts an array of integers
+ *in ascending order using the Merge sort algorithm
+ *@array: array of integers
+ *@tmp: temp array used in merge, was created outside to
+ *optimize reducing the system calls
+ *@start: fisrt element position
+ *@end: last element position
+ *Return: void
+ */
+void mergesort(int *array, int *tmp, int start, int end)
+{
+	int mid;
 
-		recursive(array, start, pivot_pos - 1, size);
-		recursive(array, pivot_pos + 1, end, size);
+	mid = (start + end) / 2;
+	if ((start + end) % 2 == 0)
+		mid--;
+	if (mid >= start)
+	{
+		mergesort(array, tmp, start, mid);
+		mergesort(array, tmp, mid + 1, end);
+		merge(array, tmp, start, mid, end);
 	}
 }
 /**
- * quick_sort - sorts an array of integers in ascending order
- * @array: elements to be sorted
- * @size: size of array
+ *merge_sort - function that sorts an array of integers
+ *in ascending order using the Merge sort algorithm
+ *@size: size of the list
+ *@array: array of integers
+ *Return: void
  */
-void quick_sort(int *array, size_t size)
+void merge_sort(int *array, size_t size)
 {
-	recursive(array, 0, size - 1, size);
+	int *tmp;
+
+	if (!array || size < 2)
+		return;
+	tmp = _calloc(size, sizeof(int));
+	mergesort(array, tmp, 0, size - 1);
+	free(tmp);
 }
 
